ef4c152c6f3d75f0cb6f68e4c8bb34e98a94a586
nathan-websculpt <websculpt.nathan@gmail.com>
2024-09-17 13:44:41 -0500

    added learning resources/links

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dead simple mockchain - Rust practice
+
+#### Rust Blockchain Resources
+
+1. https://www.youtube.com/playlist?list=PLlXBiVibng3CSYcPC_4tYUBXNjQcjgY32
+2. https://github.com/GeekLaunch/blockchain-rust
+3. https://github.com/paradigmxyz/reth
+4. https://github.com/rust-ethereum/evm
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// Rust Blockchain Resources
+// https://www.youtube.com/playlist?list=PLlXBiVibng3CSYcPC_4tYUBXNjQcjgY32
+// https://github.com/GeekLaunch/blockchain-rust
+// https://github.com/paradigmxyz/reth
+// https://github.com/rust-ethereum/evm
+//
+//
+
 fn main() {
     println!("Hello, world!");
 }
