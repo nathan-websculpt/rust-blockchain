4ac25b899816b98affec72ed9f5cec0352cbb9d1
nathan-websculpt <websculpt.nathan@gmail.com>
2024-09-17 14:02:37 -0500

    brought in notes

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,20 @@
 //
 //
 
+//from: https://youtu.be/vJdT05zl6jk?si=w-42TmYtvgnUOSSf
+// Blocks contain this information:
+
+// . Index: this block's location within the list of blocks
+// . Payload: any relevant information or events that have occurred for/in the block
+// · Timestamp: gives our blockchain a sense of time
+// . Nonce: special number used for mining (for PoW verification)
+// . Previous block hash: cryptographic fingerprint of previous block
+// . Hash: cryptographic fingerprint of all of the above data concatenated together
+
+// What is Hashing?
+// In a nutshell, a hash algorithm consists of a set of irreversible computations that can be
+// performed on a datum to generate a (usually) unique byte sequence.
+
 fn main() {
     println!("Hello, world!");
 }
